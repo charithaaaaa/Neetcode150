990b3b1d25b7226ac3b825173ee586d11afcec7d
Charitha <charithakottu@gmail.com>
2026-01-05 18:26:08 +0530

    Evaluating a reverse string using stack

---
+++ EvaluateReverseString.py
@@ -0,0 +1,19 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack=[]
+        operator={"+","-","*","/"}
+        for token in tokens:
+            if token in operator:
+                op2=stack.pop()
+                op1=stack.pop()
+                if token == "+":
+                    stack.append(op1+op2)
+                elif token == "-":
+                    stack.append(op1-op2)
+                elif token == "*":
+                    stack.append(op1*op2)
+                elif token == "/":
+                    stack.append(int(op1/op2))
+            else:
+                stack.append(int(token))
+        return (int(stack.pop()))
